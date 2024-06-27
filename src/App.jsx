00eab31a4e62fcabedc1868352414ff50c0840db
Salif Saidou <104588234+SAIDOU11@users.noreply.github.com>
2024-06-27 14:53:57 +0200

    Book animated cover
    start book animated cover

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Title from "./components/Cards/Title";
+
+const App = () => {
+  return (
+    <div>
+      <Title />
+      <h1 className="text-4xl text-blue-800">Become Good</h1>
+    </div>
+  );
+};
+
+export default App;
